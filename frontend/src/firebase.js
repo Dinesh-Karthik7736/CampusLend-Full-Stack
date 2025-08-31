cfff8c5fc2343c04b74c917e181d830d303551e3
Dinesh-Karthik7736 <158497313+Dinesh-Karthik7736@users.noreply.github.com>
2025-08-31 22:43:01 +0530

    Update firebase.js

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -5,13 +5,7 @@ import { getAuth } from 'firebase/auth';
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA1ViVJ_cjqdZf-MLMlFw4xXloIV1klUYo",
-  authDomain: "campus-lend.firebaseapp.com",
-  projectId: "campus-lend",
-  storageBucket: "campus-lend.firebasestorage.app",
-  messagingSenderId: "709333039193",
-  appId: "1:709333039193:web:a247c761545918d3905948",
-  measurementId: "G-F5Y7SYV332"
+//Your Api Details
 };
 
 const app = initializeApp(firebaseConfig);
